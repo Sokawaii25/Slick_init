06737b339776ef9e33d838cc6e76e2723801aa5e
Sokawaii <52569738+Sokawaii25@users.noreply.github.com>
2021-12-16 11:26:27 +0100

    [Dev] Séparation en classe Player

--- src/main/Main.java
+++ src/main/Main.java
@@ -14,8 +14,8 @@ public class Main extends BasicGame {
     private static final int WIDTH = 800;
     private static final int HEIGHT = 600;
     private Image character = null;
-    private Shape player = null;
-    private Shape symmetry = null;
+    private Player player = null;
+    private Player symmetry = null;
     private Vector2f p_pos = null;
     private Vector2f s_pos = null;
     private int p_size;
@@ -44,66 +44,40 @@ public class Main extends BasicGame {
         }
 
         //init variables
-        s_pos = new Vector2f(0, 0);
-        p_pos = new Vector2f(WIDTH, HEIGHT);
-        p_size = 50;
-
-        player = new Rectangle(p_pos.getX(), p_pos.getY(), p_size, p_size);
-        symmetry = new Rectangle(s_pos.getX(), s_pos.getY(), p_size, p_size);
-        character = new Image("assets/images/tungsten_block.png");
+        player = new Player(new Vector2f(0, 0), new Rectangle(0, 0, 50, 50), new Image("assets/images/tungsten_block.png"), 50);
+        symmetry = new Player(new Vector2f(WIDTH, HEIGHT), new Rectangle(WIDTH, HEIGHT, 50, 50), new Image("assets/images/tungsten_block.png"), 50);
     }
 
     @Override
     public void update(GameContainer container, int delta) {
         Input input = container.getInput();
-        int x = input.getMouseX()-p_size/2;
-        int y = input.getMouseY()-p_size/2;  //-Mouse... + window height ti go to normal
+        int x = input.getMouseX();
+        int y = input.getMouseY();  //-Mouse... + window height ti go to normal
 
-        p_pos.set(x, y);
-        player.setLocation(p_pos);
-        player.setCenterX(p_pos.getX()+p_size/2);
-        player.setCenterY(p_pos.getY()+p_size/2);
+        player.update(x, y);
+        symmetry.update(-x+WIDTH, -y+HEIGHT);
 
-        s_pos.set(-x+WIDTH-p_size, -y+HEIGHT-p_size);
-        symmetry.setLocation(s_pos);
-        symmetry.setCenterX(s_pos.getX()+p_size/2);
-        symmetry.setCenterY(s_pos.getY()+p_size/2);
-
-        if(rotate) {
-            player = player.transform(Transform.createRotateTransform((float) Math.toRadians(5), player.getCenterX(), player.getCenterY()));
-            symmetry = symmetry.transform(Transform.createRotateTransform((float) Math.toRadians(5), symmetry.getCenterX(), symmetry.getCenterY()));
-        }
-
-        collides = player.intersects(symmetry);
+        collides = player.collides(symmetry);
     }
 
     @Override
     public void render(GameContainer container, Graphics g) {
         if (collides) {
-            g.setColor(Color.red);
-            character.drawFlash(s_pos.getX()-1, s_pos.getY()-1, p_size+2, p_size+2);
-            g.setColor(Color.white);
+            player.hits(symmetry);
             collides = false;
         }
-        character.draw(p_pos.getX(), p_pos.getY(), p_size, p_size);
-        character.draw(s_pos.getX(), s_pos.getY(), p_size, p_size);
 
         g.setColor(Color.white);
-        g.draw(player);
+        player.render(g);
         g.setColor(Color.green);
-        g.draw(symmetry);
-        //rotation
-        if(rotate) {
-            character.setCenterOfRotation(p_size / 2, p_size / 2);
-            character.rotate(5);
-        }
+        symmetry.render(g);
     }
 
     @Override
     public void mouseClicked(int button, int x, int y, int clickCount) {
         System.out.println("x: " + x + " y: " + y);
-        System.out.println("x: " + p_pos.getX() + " y: " + p_pos.getY());
-        rotate = !rotate;
+        player.toggleRotation();
+        symmetry.toggleRotation();
     }
 
     public static void main(String[] args) throws SlickException {
--- src/main/Player.java
+++ src/main/Player.java
@@ -1,49 +1,81 @@
 package main;
 
-import org.newdawn.slick.Color;
+import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.geom.Shape;
+import org.newdawn.slick.geom.Transform;
 import org.newdawn.slick.geom.Vector2f;
 
 public class Player {
     private Vector2f pos;
+
     private Shape shape;
     private Image image;
     private int size;
-    private boolean rotate;
+    private boolean rotation;
 
-    public Player(Vector2f pos, Shape shape, int size) {
+    public Player(Vector2f pos, Shape shape, Image image, int size) {
         this.pos = pos;
         this.shape = shape;
+        this.image = image;
         this.size = size;
     }
 
-    public Player(int x, int y, Shape shape, int size) {
+    public Player(int x, int y, Shape shape, Image image, int size) {
         this.pos = new Vector2f(x, y);
         this.shape = shape;
+        this.image = image;
         this.size = size;
+    }
+
+
+    public Shape getShape() {
+        return this.shape;
+    }
+
+    public Image getImage() {
+        return this.image;
     }
 
     public boolean collides(Shape other) {
         return this.shape.intersects(other);
     }
 
-    public void toggleRotate(boolean) {
+    public boolean collides(Player other) {
+        return this.shape.intersects(other.getShape());
+    }
 
+    public void toggleRotation() {
+        this.rotation = !this.rotation;
     }
 
     public void update(int x, int y) {
-        pos.set(x, y);
+        pos.set(x-size/2, y-size/2);
         this.shape.setLocation(pos);
         this.shape.setCenterX(pos.getX()+size/2);
         this.shape.setCenterY(pos.getY()+size/2);
+
+        if(rotation) {
+            this.shape = this.shape.transform(Transform.createRotateTransform((float) Math.toRadians(5), this.shape.getCenterX(), this.shape.getCenterY()));
+            this.shape = this.shape.transform(Transform.createRotateTransform((float) Math.toRadians(5), this.shape.getCenterX(), this.shape.getCenterY()));
+        }
     }
 
-    public void render() {
+    public void hits(Player other) {
+        other.isHit();
+    }
+
+    public void isHit() {
+        this.image.drawFlash(pos.getX()-2, pos.getY()-2, size+4, size+4);
+    }
+
+    public void render(Graphics g) {
         this.image.draw(pos.getX(), pos.getY(), size, size);
 
+        g.draw(this.shape);
+
         //rotation
-        if(rotate) {
+        if(this.rotation) {
             this.image.setCenterOfRotation(size / 2, size / 2);
             this.image.rotate(5);
         }
